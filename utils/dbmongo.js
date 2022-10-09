34824e324f37a42b2743791cd1b492436d560c61
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-10-09 23:14:07 +0200

    six-deploy

--- utils/dbmongo.js
+++ utils/dbmongo.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 require('dotenv').config();
 
 
+
+
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true, ssl: true});
 const db = mongoose.connection;
 
