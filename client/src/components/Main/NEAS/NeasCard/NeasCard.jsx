d3668adf87b2f4c707b97700ed26e75611728674
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-11-20 19:56:40 +0100

    add/logic-cart-nea

--- client/src/components/Main/NEAS/NeasCard/NeasCard.jsx
+++ client/src/components/Main/NEAS/NeasCard/NeasCard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useContext} from "react";
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
@@ -9,6 +9,8 @@ import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import NeasEditForm from '../NeasEditForm';
 import CardMedia from '@mui/material/CardMedia';
+import { loginContext } from '../../../../context/loginContext';
+import { useDispatch } from 'react-redux';
 
 
 
@@ -27,34 +29,43 @@ const style = {
 
 function NeasCard(props) {
 
-const nea = props.data
-const [open, setOpen] = React.useState(false);
-
-const handleOpen = () => {setOpen(true)}
-const handleClose = () => {setOpen(false)}
 
 
-const removeNea = async () =>{
-  try {
-    const res = await axios.delete(`https://sleepy-retreat-77024.herokuapp.com/api/astronomy/neas/delete/${nea.designation}`);
-    const data = await res.data;
-    console.log(data);
-    props.remove();
-  } catch (e) {
-    console.log(e);
-  }   
-}
+  const precision = 10;
+  let priceItem = Math.floor(Math.random() * (10 * precision  - 1 * precision) + 1 * precision) / (10*precision)
 
-// h_mag: props.h_mag,
-//         moid_au: props.moid_au,
-//         q_au_1: props.q_au_1,
-//         q_au_2: props.q_au_2,
-//         i_deg: props.i_deg,
-//         pha: props.pha,
+  const nea = {...props.data, price:priceItem}
+  const [open, setOpen] = React.useState(false);
+
+  const { loginData } = useContext(loginContext)
+
+  const handleOpen = () => { setOpen(true) }
+  const handleClose = () => { setOpen(false) }
+
+  const dispatch = useDispatch();
+
+
+  const removeNea = async () => {
+    try {
+      const res = await axios.delete(`https://sleepy-retreat-77024.herokuapp.com/api/astronomy/neas/delete/${nea.designation}`);
+      const data = await res.data;
+      console.log(data);
+      props.remove();
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  // h_mag: props.h_mag,
+  //         moid_au: props.moid_au,
+  //         q_au_1: props.q_au_1,
+  //         q_au_2: props.q_au_2,
+  //         i_deg: props.i_deg,
+  //         pha: props.pha,
   return (
     <div>
 
-<div className="modal">
+      <div className="modal">
         <Modal
           keepMounted
           open={open}
@@ -63,39 +74,45 @@ const removeNea = async () =>{
           aria-describedby="keep-mounted-modal-description"
         >
           <Box sx={style}>
-            <NeasEditForm handleClose={handleClose} img={nea.img} h_mag={nea.h_mag} moid_au={nea.moid_au} q_au_1={nea.q_au_1} q_au_2={nea.q_au_2} i_deg={nea.i_deg} pha={nea.pha}  designation={nea.designation} discovery_date={nea.discovery_date} period_yr={nea.period_yr} orbit_class={nea.orbit_class} />
+            <NeasEditForm handleClose={handleClose} img={nea.img} h_mag={nea.h_mag} moid_au={nea.moid_au} q_au_1={nea.q_au_1} q_au_2={nea.q_au_2} i_deg={nea.i_deg} pha={nea.pha} designation={nea.designation} discovery_date={nea.discovery_date} period_yr={nea.period_yr} orbit_class={nea.orbit_class} />
           </Box>
         </Modal>
       </div>
-            <Card sx={{ minWidth: 290, margin: 1 }}>
-              <CardMedia
-                component="img"
-                height="140"
-                image={nea.img}
-                alt="green iguana"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="div">
-                  {nea.designation}
-                </Typography>
-                {/* <Typography variant="body2" color="text.secondary">
+      <Card sx={{ minWidth: 290, margin: 1 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={nea.img}
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {nea.designation}
+          </Typography>
+          {/* <Typography variant="body2" color="text.secondary">
                   - Designación: {nea.designation}
                 </Typography> */}
-                <Typography variant="body2" color="text.secondary">
-                  - Fecha: {String(nea.discovery_date).slice(0,10)}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  - Periodo/Año:{nea.period_yr}
-                </Typography>
-                <Typography variant="body2" color="text.secondary">
-                  - Clase de órbita: {nea.orbit_class}
-                </Typography>
-              </CardContent>
-              <CardActions>
-                <Button size="small" onClick={handleOpen}>Editar</Button>
-                <Button size="small" onClick={removeNea}>Borrar</Button>
-              </CardActions>
-            </Card>
+          <Typography variant="body2" color="text.secondary">
+            - Fecha: {String(nea.discovery_date).slice(0, 10)}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            - Periodo/Año:{nea.period_yr}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            - Clase de órbita: {nea.orbit_class}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" onClick={handleOpen}>Editar</Button>
+          <Button size="small" onClick={removeNea}>Borrar</Button>
+          {loginData ? <Button size="small" onClick={() => {
+            dispatch({
+              type: "ADD_CART",
+              payload: nea
+            })
+          }} >Añadir al carro</Button> : null}
+        </CardActions>
+      </Card>
     </div>
   )
 }
--- client/src/redux/reducers/reducers.js
+++ client/src/redux/reducers/reducers.js
@@ -1,88 +1,121 @@
 const initProduct = {
-    numberCart:0,
-    Carts:[],
-    _products:[]
+    numberCart: 0,
+    Carts: [],
+    _products: []
 }
 
 const precision = 10;
-let priceItem = Math.floor(Math.random() * (10 * precision  - 1 * precision) + 1 * precision) / (10*precision)
+let priceItem = Math.floor(Math.random() * (10 * precision - 1 * precision) + 1 * precision) / (10 * precision)
 
 function shopping(state = initProduct, action) {
     switch (action.type) {
         case "GET_ALL_PRODUCTS":
-            return{
+            return {
                 ...state,
                 _products: action.payload
             }
 
         case "ADD_CART":
-            if(state.numberCart===0){
-                let cart = {
-                    name:action.payload.name,
-                    mass: action.payload.mass,
-                    quantity:1,
-                    image:action.payload.img,
-                    price:action.payload.price,
-                } 
-                state.Carts.push(cart); 
-            } else{
+            if (state.numberCart === 0) {
+                if (action.payload.orbit_class) {
+                    let cart = {
+                        name: action.payload.designation,
+                        quantity: 1,
+                        image: action.payload.img,
+                        price: action.payload.price,
+                    }
+                    state.Carts.push(cart);
+                }
+                else {
+                    let cart = {
+                        name: action.payload.name,
+                        mass: action.payload.mass,
+                        quantity: 1,
+                        image: action.payload.img,
+                        price: action.payload.price,
+                    }
+                    state.Carts.push(cart);
+                }
+
+            } else {
                 let check = false;
-                state.Carts.map((item,i)=>{
-                    if(item.name===action.payload.name){
-                        state.Carts[i].quantity++;
-                        check=true;
+                state.Carts.map((item, i) => {
+                    if (action.payload.orbit_class){
+                        if (item.name === action.payload.designation) {
+                            state.Carts[i].quantity++;
+                            check = true;
+                        }
                     }
+                    if (action.payload.name) {
+                        if (item.name === action.payload.name) {
+                            state.Carts[i].quantity++;
+                            check = true;
+                        }   
+                    }
+
                 });
-                if(!check){
-                    let _cart = {
-                        name:action.payload.name,
-                        quantity:1,
-                        mass:action.payload.mass,
-                        image:action.payload.img,
-                        price:action.payload.price
+                if (!check) {
+
+                    if (action.payload.orbit_class) {
+                        let cart = {
+                            name: action.payload.designation,
+                            quantity: 1,
+                            image: action.payload.img,
+                            price: action.payload.price,
+                        }
+                        state.Carts.push(cart);
                     }
-                    state.Carts.push(_cart);
+                    else {
+                        let cart = {
+                            name: action.payload.name,
+                            mass: action.payload.mass,
+                            quantity: 1,
+                            image: action.payload.img,
+                            price: action.payload.price,
+                        }
+                        state.Carts.push(cart);
+                    }
                 }
             }
-            return{
+            return {
                 ...state,
-                numberCart:state.numberCart +1
+                numberCart: state.numberCart + 1
             }
 
         case "INCREASE_QUANTITY":
             state.Carts[action.payload].quantity++;
-            return{
+            return {
                 ...state,
                 Carts: state.Carts,
-                numberCart:state.numberCart +1
+                numberCart: state.numberCart + 1
             }
 
         case "DECREASE_QUANTITY":
             let qty = state.Carts[action.payload].quantity;
-            if(qty>1){
+            if (qty > 1) {
                 state.Carts[action.payload].quantity--;
-                return{
+                return {
                     ...state,
                     Carts: state.Carts,
-                    numberCart:state.numberCart -1
+                    numberCart: state.numberCart - 1
 
                 }
-            } else{
-                state.Carts[action.payload].quantity=0;
-                return{
+            } else {
+                state.Carts[action.payload].quantity = 0;
+                return {
                     ...state,
-                    Carts:state.Carts.filter(item=>item.name!==state.Carts[action.payload].name),
-                    numberCart:state.numberCart -1
- 
+                    Carts: state.Carts.filter(item => item.name !== state.Carts[action.payload].name),
+                    numberCart: state.numberCart - 1
+
                 }
             }
 
         case "DELETE_CART":
             let quantity_ = state.Carts[action.payload].quantity;
-            return{
+            return {
                 ...state,
-                numberCart:state.numberCart - quantity_,
-                Carts:state.Carts.filter(item=>item.name!==state.Carts[action.payload].name)   
+                numberCart: state.numberCart - quantity_,
+                Carts: state.Carts.filter(item => item.name !== state.Carts[action.payload].name)
             }
 
         default:
