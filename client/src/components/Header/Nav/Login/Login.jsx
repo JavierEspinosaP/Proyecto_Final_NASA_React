9e0009d31effdf127d08cb39ac8310b349c5f8d1
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-11-21 16:04:03 +0100

    fix/google-button-in-form

--- client/src/components/Header/Nav/Login/Login.jsx
+++ client/src/components/Header/Nav/Login/Login.jsx
@@ -1,27 +1,24 @@
 import React, { useContext,  useEffect, useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
-import { useForm } from 'react-hook-form';
+import loginSound from '../../../../assets/sounds/login complete.wav'
 import axios from 'axios';
 import { loginContext } from '../../../../context/loginContext'
 import Swal from 'sweetalert2/dist/sweetalert2.js';
 import bcrypt from 'bcryptjs'
 import useSound from 'use-sound';
-import loginSound from '../../../../assets/sounds/login complete.wav'
 import closedEye from '../../../../assets/closedEye.png'
 import openEye from '../../../../assets/openEye.png'
-import registeredUser from '../../../../assets/sounds/registeredUser.wav'
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+
+import jwt_decode from "jwt-decode";
+
+import Register from './Register'
+
 
 
 // const regex = ^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[*.!@$%^&(){}[]:;<>,.?/~_+-=|\]).{8,32}$
 
-const schema = yup.object({
-  nickname: yup.string().min(3).required(),
-  email: yup.string().email().required(),
-  password: yup.string(),
-}).required();
+
 
 const Login = (props) => {
 
@@ -32,18 +29,15 @@ const Login = (props) => {
   const [passwordShown, setPasswordShown] = useState(false);
   const [registerForm, setRegisterForm] = useState(false)
 
-  const [registerError, setRegisterError] = useState(false)
-
   const [loginError, setLoginError] = useState(false)
-
-  const [repPasswordError, setRepPasswordError] = useState(false)
-
+  const [change] = useSound(loginSound, { volume: 0.5 });
 
 
 
-  const nicknameError = 'El nickname es requerido y es necesario que contenga 3 o más caracteres'
-  const passwordError = 'La password debe contener al menos una mayúscula, una minúscula, un número y un símbolo'
-  const emailError = 'El email debe contener un formato correcto'
+  const [userGoogle, setUserGoogle] = useState({})
+
+
+
 
   // Password toggle handler
   const togglePassword = () => {
@@ -59,145 +53,94 @@ const Login = (props) => {
   };
 
 
-  const [change] = useSound(loginSound, { volume: 0.5 });
-
-  const [registered] = useSound(registeredUser, { volume: 0.5 });
-
-
-
   
+  function handleCallbackResponse(response){
+    console.log("Encoded JWT ID token: " + response.credential);
+    const userObject = jwt_decode(response.credential);
+    setUserGoogle(userObject)
+  }
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: "741529201651-87iqkt4276347cbbfvc4vd5mh3mpiufv.apps.googleusercontent.com",
+      callback: handleCallbackResponse
+    });
+
+    google.accounts.id.renderButton(
+      document.getElementById("signInDiv"),
+      {theme: "dark", size: "large"}
+    )
+  }, []) 
 
 
 
   const loginValidation = async (user) => {
 
-      console.log(user);    
-    try {
+    console.log(user);    
+  try {
 
-      const res = await axios.get(`https://sleepy-retreat-77024.herokuapp.com/api/users?email=${user.email}`)
-      const data = await res.data;
-      console.log(res.data.length);
-      if (res.data.length===0) {
-        setLoginError(true)
-        console.log(loginError);
-      }
-      const password = user.password
-      console.log(password);
-      console.log(data);
-      const validPass = await bcrypt.compare(password, data[0].password)
-      console.log(validPass);
-      if (validPass) {
-
-        setLoginData(true)
-        Swal.fire({
-          icon: 'success',
-          title: 'Login complete',
-          showConfirmButton: false,
-          timer: 2000
-        })
-        change()
-
-      }
-      if (res.data.length===0) {
-        setLoginError(true)
-        console.log(loginError);
-      }
+    const res = await axios.get(`https://sleepy-retreat-77024.herokuapp.com/api/users?email=${user.email}`)
+    const data = await res.data;
+    console.log(res.data.length);
+    if (res.data.length===0) {
+      setLoginError(true)
+      console.log(loginError);
     }
-    catch(e){
-      console.log(e);
+    const password = user.password
+    console.log(password);
+    console.log(data);
+    const validPass = await bcrypt.compare(password, data[0].password)
+    console.log(validPass);
+    if (validPass) {
+
+      setLoginData(true)
+      Swal.fire({
+        icon: 'success',
+        title: 'Login complete',
+        showConfirmButton: false,
+        timer: 2000
+      })
+      change()
+
+    }
+    if (res.data.length===0) {
+      setLoginError(true)
+      console.log(loginError);
     }
   }
-
-  const sendForm = (event) => {
-    event.preventDefault()
-    const email = event.target.email.value
-    const password = event.target.password.value
-    const user = {email, password}
-
-    loginValidation(user)
+  catch(e){
+    console.log(e);
   }
+}
 
-  const registration = async (user) => {
+const sendForm = (event) => {
+  event.preventDefault()
+  const email = event.target.email.value
+  const password = event.target.password.value
+  const user = {email, password}
 
-    const hash = await bcrypt.hash(user.password, 10)
-
-    const userCrypt = {
-      nickname: user.nickname,
-      email: user.email,
-      password: hash
-    }
+  loginValidation(user)
+}
 
 
-    console.log(userCrypt);
-    console.log(user.passwordRepeat);
-    if (user.password == user.passwordRepeat) {
-    try{
-      const res = await axios.post("https://sleepy-retreat-77024.herokuapp.com/api/users/create", userCrypt)
-      const data = await res.data;
-      console.log(data.Answer);
-      if (data.Answer === "User created") {
-        setLoginData(true)
-        Swal.fire({
-          icon: 'success',
-          title: 'Registered user',
-          showConfirmButton: false,
-          timer: 3000
-        })
 
-        
-        registered()
-      }
-    else{
-      setRegisterError(true)
-    }
-    }
-    catch(e){
-      console.log(e);
-    }      
-    }
-    else{
-      setRepPasswordError(true)
-    }
 
-  }
-
-  const { register, handleSubmit, formState: { errors } } = useForm(
-    {resolver: yupResolver(schema)});
 
   return (<div className="formContainer">
-    {registerForm ? 
-    <form onSubmit={handleSubmit(registration)}>
-      <h4 className="landingName">Introduce tus datos</h4>
-      {registerError?<p className="pError">El email introducido ya está en uso</p>:null}
-      <TextField className="registerInput" name="nickname" {...register("nickname")} placeholder="Nickname  " />
-      <p className="pError">{errors.nickname?nicknameError:null}</p>
-      <TextField className="registerInput" name="email" {...register("email")} placeholder="Email  " />
-      <p className="pError">{errors.email?emailError:null}</p>
-      <div>
-      <TextField className="registerInput" name="password" type={passwordShown ? "text" : "password"} {...register("password")} placeholder="Password" />
-      <Button id={registerForm ?"visibilityRegister":"visibility"} onClick={togglePassword}><img id="eye" src={passwordShown ? openEye : closedEye} alt="eye" /></Button>         
-      </div>
-      <p className="pError">{errors.password?passwordError:null}</p>   
-      <div>
-      <TextField className="registerInput" name="passwordRepeat" {...register("passwordRepeat")} type={passwordShown ? "text" : "password"} placeholder="Repite password" />    
-      {repPasswordError?<p className="pError">Las contraseñas deben coincidir</p>: null}    
-        </div>  
-      <Button className="registerInput" type="submit" variant="contained">Submit</Button>
-      <p id="registerP">Para loguearte, click <Button onClick={toggleRegister}><p>aquí</p></Button></p>
-    </form>
-
-      : <form onSubmit={sendForm}>
-        <h4 className="landingName">Introduce tus credenciales</h4>
-        {loginError?<p className="pError">Email o contraseña incorrectos</p>: null}        
-        <TextField className="loginInput" name="email" placeholder="Email  " />
-        <div>
-        <TextField className="loginInput" name="password" type={passwordShown ? "text" : "password"} placeholder="Password  " />
-        <Button id={registerForm ?"visibilityRegister":"visibility"} onClick={togglePassword}><img id="eye" src={passwordShown ? openEye : closedEye} alt="eye" /></Button>          
-        </div>
-        <Button className="loginInput" type="submit" variant="contained">Submit</Button>
-        <p id="registerP">Si aun no tienes cuenta, click <Button onClick={toggleRegister}><p>aquí</p></Button></p>
-
-      </form>}
+    {registerForm ? <Register/>: 
+<form onSubmit={sendForm}>
+<h4 className="landingName">Introduce tus credenciales</h4>
+{loginError ? <p className="pError">Email o contraseña incorrectos</p> : null}
+<TextField className="loginInput" name="email" placeholder="Email  " />
+<div>
+  <TextField className="loginInput" name="password" type={passwordShown ? "text" : "password"} placeholder="Password  " />
+  <Button id={registerForm ? "visibilityRegister" : "visibility"} onClick={togglePassword}><img id="eye" src={passwordShown ? openEye : closedEye} alt="eye" /></Button>
+</div>
+<Button className="loginInput" type="submit" variant="contained">Submit</Button>
+<p id="registerP">Si aun no tienes cuenta, click <Button onClick={toggleRegister}><p>aquí</p></Button></p>
+<div id="signInDiv"></div>
+</form>}
   </div>)
 }
 
---
+++ client/src/components/Header/Nav/Login/Register/Register.jsx
@@ -0,0 +1,136 @@
+import React, {useState, useEffect, useContext} from 'react'
+
+import useSound from 'use-sound';
+import closedEye from '../../../../../assets/closedEye.png'
+import openEye from '../../../../../assets/openEye.png'
+import Swal from 'sweetalert2/dist/sweetalert2.js';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import axios from 'axios';
+import bcrypt from 'bcryptjs'
+import { loginContext } from '../../../../../context/loginContext'
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from 'react-hook-form';
+import registeredUser from '../../../../../assets/sounds/registeredUser.wav'
+import Login from '../../Login'
+
+function Register(props) {
+
+  const schema = yup.object({
+    nickname: yup.string().min(3).required(),
+    email: yup.string().email().required(),
+    password: yup.string(),
+  }).required();
+
+  const { setLoginData } = useContext(loginContext)
+
+
+  const [passwordShown, setPasswordShown] = useState(false);
+  const [registerForm, setRegisterForm] = useState(true)
+
+  const [registerError, setRegisterError] = useState(false)
+
+  const [repPasswordError, setRepPasswordError] = useState(false)
+
+  const nicknameError = 'El nickname es requerido y es necesario que contenga 3 o más caracteres'
+  const passwordError = 'La password debe contener al menos una mayúscula, una minúscula, un número y un símbolo'
+  const emailError = 'El email debe contener un formato correcto'
+
+
+  const [registered] = useSound(registeredUser, { volume: 0.5 });
+
+
+    // Password toggle handler
+    const togglePassword = () => {
+      // When the handler is invoked
+      // inverse the boolean state of passwordShown
+      setPasswordShown(!passwordShown);
+    };
+  
+    const toggleRegister = () => {
+      // When the handler is invoked
+      // inverse the boolean state of passwordShown
+      setRegisterForm(!registerForm);
+    };
+
+
+
+    const registration = async (user) => {
+
+      const hash = await bcrypt.hash(user.password, 10)
+  
+      const userCrypt = {
+        nickname: user.nickname,
+        email: user.email,
+        password: hash
+      }
+  
+  
+      console.log(userCrypt);
+      console.log(user.passwordRepeat);
+      if (user.password == user.passwordRepeat) {
+      try{
+        const res = await axios.post("https://sleepy-retreat-77024.herokuapp.com/api/users/create", userCrypt)
+        const data = await res.data;
+        console.log(data.Answer);
+        if (data.Answer === "User created") {
+          setLoginData(true)
+          Swal.fire({
+            icon: 'success',
+            title: 'Registered user',
+            showConfirmButton: false,
+            timer: 3000
+          })
+  
+          
+          registered()
+        }
+      else{
+        setRegisterError(true)
+      }
+      }
+      catch(e){
+        console.log(e);
+      }      
+      }
+      else{
+        setRepPasswordError(true)
+      }
+  
+    }
+
+
+
+
+
+
+const { register, handleSubmit, formState: { errors } } = useForm(
+  {resolver: yupResolver(schema)});
+
+  return (
+    <div>
+      {registerForm?<form onSubmit={handleSubmit(registration)}>
+      <h4 className="landingName">Introduce tus datos</h4>
+      {registerError?<p className="pError">El email introducido ya está en uso</p>:null}
+      <TextField className="registerInput" name="nickname" {...register("nickname")} placeholder="Nickname  " />
+      <p className="pError">{errors.nickname?nicknameError:null}</p>
+      <TextField className="registerInput" name="email" {...register("email")} placeholder="Email  " />
+      <p className="pError">{errors.email?emailError:null}</p>
+      <div>
+      <TextField className="registerInput" name="password" type={passwordShown ? "text" : "password"} {...register("password")} placeholder="Password" />
+      <Button id={registerForm ?"visibilityRegister":"visibility"} onClick={togglePassword}><img id="eye" src={passwordShown ? openEye : closedEye} alt="eye" /></Button>         
+      </div>
+      <p className="pError">{errors.password?passwordError:null}</p>   
+      <div>
+      <TextField className="registerInput" name="passwordRepeat" {...register("passwordRepeat")} type={passwordShown ? "text" : "password"} placeholder="Repite password" />    
+      {repPasswordError?<p className="pError">Las contraseñas deben coincidir</p>: null}    
+        </div>  
+      <Button className="registerInput" type="submit" variant="contained">Submit</Button>
+      <p id="registerP">Para loguearte, click <Button onClick={toggleRegister}><p>aquí</p></Button></p>
+    </form>:<Login/>}
+    </div>
+  )
+}
+
+export default Register
