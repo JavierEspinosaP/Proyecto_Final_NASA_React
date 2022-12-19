8ab69346b1fe75ce117f748b15f2278ed15b9964
Javi Espinosa <103537170+JavierEspinosaP@users.noreply.github.com>
2022-12-19 16:37:10 +0100

    external space invaders

--- client/src/components/Main/Game/Dashboard/Dashboard.jsx
+++ client/src/components/Main/Game/Dashboard/Dashboard.jsx
@@ -1,27 +1,12 @@
-import React, {useRef, useEffect} from 'react'
-import spaceship from '../../../../assets/spaceship2.png'
+import React, { useState, useEffect, useRef } from 'react';
+import { IFrame } from './iframe'
 
 function Dashboard() {
 
-  const canvasRef = useRef(null);
 
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext('2d');
-    console.log(ctx);
-    ctx.imageSmoothingEnabled = false;
-    ctx.imageSmoothingQuality = 'high';
-    
-
-    const image = new Image();
-    image.src = spaceship;
-    image.onload = () => {
-      ctx.drawImage(image, 185, 550, 30, 30);
-    };
-  }, []);
-
-
-  return (<canvas width={400} height={600} ref={canvasRef} className="dashboardGame"></canvas>)
+  return (
+    <iframe src="https://space-invaders.throb.repl.co/" width="800" height="900" frameborder="0"></iframe>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
